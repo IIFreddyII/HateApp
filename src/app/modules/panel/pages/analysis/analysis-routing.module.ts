35a068ad38bb1f0906b9e46dc1589b4d7ed6a2d0
Alfredo Luna Castro <alfredo.luna@swopyn.com>
2023-05-21 14:09:17 -0600

    navegacion

--- src/app/modules/panel/pages/analysis/analysis-routing.module.ts
+++ src/app/modules/panel/pages/analysis/analysis-routing.module.ts
@@ -13,11 +13,11 @@ const routes: Routes = [
         path: 'feeling', component: FeelingsComponent 
       },
       {
-        path: 'gender', component: GenderComponent,
+        path: 'algorithm', component: GenderComponent,
         loadChildren: () => import('./components/gender/gender.module').then(m => m.GenderModule)
       },
       {
-        path: 'word', component: WordsComponent 
+        path: 'wordcloud', component: WordsComponent 
       },
       {
         path: 'result', component: ResultComponent 
